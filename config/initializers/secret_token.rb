4792bfc3183a7b282f3d2a787bd520c04e747584
crank <crank@crank.(none)>
2013-03-07 17:03:03 -0300

    [Iniciando Maquetacion de Coketas]

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Coketas::Application.config.secret_token = '1b3ee5e1efc8d0619570952f21f3ab21c0f2567681a68f95a1ffdcac778d2de14431f8c6a266be13b311ded7c51f5fffa66b1030d641e5f80536e5a235cee800'
