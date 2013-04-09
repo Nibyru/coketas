cbe0780c4284902a56b6a0766b6626d9401d99f3
sergio sebastian arriagada <sergio@nibyru.com>
2013-04-09 20:06:18 -0300

    [configuracion general]   -se configuro la base de datos   -se agrego la gema paperclip

--- config/application.rb
+++ config/application.rb
@@ -54,9 +54,9 @@ module Coketas
     # parameters by using an attr_accessible or attr_protected declaration.
     config.active_record.whitelist_attributes = true
 
+    config.assets.initialize_on_precompile = false
     # Enable the asset pipeline
     config.assets.enabled = true
-    config.assets.initialize_on_precompile = false
 
 
     # Version of your assets, change this if you want to expire all your assets
