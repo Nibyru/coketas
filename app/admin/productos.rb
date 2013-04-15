a9a8358a567e2e5c7838f84ce1cadc290f4f16ea
Crank <mario@nibyru.com>
2013-04-15 20:19:55 -0300

    [Se modifico el formato del precio]

--- app/admin/productos.rb
+++ app/admin/productos.rb
@@ -1,4 +1,16 @@
 ActiveAdmin.register Producto do
+	index :selectable => true do
+		column :codigo
+		column :nombre
+		column :marca
+		column :descripcion
+		column :stock
+		column :precio do |producto|
+	      number_to_currency producto.precio, :unit => "$", :format => "%u%n"
+	    end
+
+		default_actions
+	end
 	form do |f|
 	  f.inputs "Detalles" do
 	    f.input :marca, :collection => Marca.all.map{ |m| [m.nombre, m.id] }
@@ -7,11 +19,9 @@ ActiveAdmin.register Producto do
 	    f.input :descripcion
 	    f.input :stock
 	    f.input :precio
-	    f.input :talle, :collection => Talle.all.map{ |t| [t.nombre, t.id] }
-	    f.input :color, :collection => Color.all.map{ |c| [c.nombre, c.id] }
+	    f.input :talle, as: :check_boxes, :collection => Talle.all.map{ |t| [t.nombre, t.id] }
+	    f.input :color, as: :check_boxes, :collection => Color.all.map{ |c| [c.nombre, c.id] }
 	  end
 	  f.actions
 	end
-	show :title => :nombre do
-  	end
 end
