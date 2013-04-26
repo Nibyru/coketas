a17198007a02e10dd7618555d1fd655d4d3f6134
Sergio Sebastian Arriagada <sergio@nibyru.com>
2013-04-25 21:55:11 -0300

    [paperclip]   -se configuro dropbox

--- app/models/producto.rb
+++ app/models/producto.rb
@@ -11,5 +11,11 @@ class Producto < ActiveRecord::Base
   accepts_nested_attributes_for :talles
   accepts_nested_attributes_for :colors
 
-  has_attached_file :foto, :styles => { :medium => "624x458>", :thumb => "202x148>", :min => "100x85>" }
+  has_attached_file :foto,
+  	:styles => { :medium => "624x458>", :thumb => "202x148>", :min => "100x85>" },
+    :storage => :dropbox,
+    :dropbox_credentials => "#{Rails.root}/config/dropbox.yml",
+    :dropbox_options => {
+      :path => proc { |style| "#{style}/#{id}_#{foto.original_filename}" }
+    }
 end
