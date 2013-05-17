cf0507e24890c97d401e5d37d76ef9ad2dbe8581
Sergio Sebastian Arriagada <sarriagada87@gmail.com>
2013-05-17 18:46:42 -0300

    [menu]   -se modifico el estilo del menu para los links actuales

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,2 @@
 module ApplicationHelper
-	def link_to_menu(destino)
-		if destino == "stock"
-			link_to_unless_current(content_tag(:li, "#{destino}", :class => "stock"), { :action => "#{destino}" })
-		else
-			link_to_unless_current(content_tag(:li, destino=="index" ? "inicio" : "#{destino}"), { :action => "#{destino}" })
-		end
-	end
 end
