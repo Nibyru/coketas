f828c7c16e458f132acb127f45111f730929b743
Sergio Sebastián Arriagada <sarriagada87@gmail.com>
2013-06-06 21:31:25 -0300

    [config actionmailer]

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Coketas::Application.initialize!
+
+ActionMailer::Base.smtp_settings = YAML.load_file(
+    Rails.root.join('config', 'mailers.yml'))[Rails.env].try(:to_options)
