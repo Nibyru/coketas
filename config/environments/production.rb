471bd8f00e2fc9140c68f7c27ec1a93ae08fc0cb
Sergio Sebastian Arriagada <sarriagada87@gmail.com>
2013-08-20 23:34:44 -0300

    [se modifico url host]

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,4 +68,6 @@ Coketas::Application.configure do
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
   # ActionMailer Config
   # Setup for production - deliveries, no errors raised
+  config.action_mailer.default_url_options = { :host => 'coketas.com' }
+  config.action_mailer.delivery_method = :smtp
 end
