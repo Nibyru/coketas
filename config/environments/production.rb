15765990e281cbaefcd3e24a8d26a9bb9f64bde2
Sergio Sebastian Arriagada <sarriagada87@gmail.com>
2013-08-20 23:16:32 -0300

    [se modifico production enviroment]

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,8 +66,6 @@ Coketas::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
-  config.action_mailer.default_url_options = { host: 'prueba-coketas.herokuapp.com' }
   # ActionMailer Config
   # Setup for production - deliveries, no errors raised
-  config.action_mailer.delivery_method = :smtp
 end
