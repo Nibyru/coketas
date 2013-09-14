9dc096e1408bda120c54816d906c76a2ac42f22c
Seba <seba@seba-Aspire-Z3101.(none)>
2013-09-13 23:33:04 -0300

    [se modifico la configuracion de mailchimp]

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,8 +1,6 @@
 ActionMailer::Base.smtp_settings = {
   address:              'smtp.mandrillapp.net',
   port:                 '587',
-  authentication:       :plain,
   user_name:            ENV['MANDRIL_USERNAME'],
   password:             ENV['MANDRIL_PASSWORD'],
-  enable_starttls_auto: true
 }
